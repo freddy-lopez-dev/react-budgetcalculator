3b7dc3e26d27e61eac6e67ad8407643dda1de101
Frederick Lopez <mtester010@gmail.com>
2022-05-13 19:29:34 -0500

    Add functionality to enable controlled form component

--- src/components/BottomComponents/AddTransactForm.jsx
+++ src/components/BottomComponents/AddTransactForm.jsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
+
 const AddTransactForm = (props) => {
+  const [inputDescription, setDescription] = useState("");
+  const [inputAmount, setAmount] = useState("");
+
+  const addDescriptionHandler = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const addAmountHandler = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const onClickHandler = () => {
+    props.addTransaction(inputDescription, inputAmount);
+  };
+
   return (
     <div className="add">
       <div className="add__container">
         <input
+          onChange={addDescriptionHandler}
           type="text"
           className="add__description"
           placeholder="Add description"
         />
-        <input type="number" className="add__value" placeholder="Value" />
-        <button className="add__btn">
+        <input
+          type="number"
+          className="add__value"
+          placeholder="Value"
+          onChange={addAmountHandler}
+        />
+        <button className="add__btn" onClick={onClickHandler}>
           <i className="ion-ios-checkmark-outline"></i>
         </button>
       </div>
--- src/components/BottomComponents/Bottom.jsx
+++ src/components/BottomComponents/Bottom.jsx
@@ -1,11 +1,43 @@
 import ExpenseList from "./ExpenseList";
 import AddTransactForm from "./AddTransactForm";
 import IncomeList from "./IncomeList";
+import { useState } from "react";
 
 const Bottom = (props) => {
+  const dateToday = new Date();
+  const formatDate = dateToday.toLocaleDateString("en-CA");
+  const initialTransactionList = [
+    {
+      id: 0,
+      description: "My salary",
+      amount: 3000.0,
+      Date: formatDate,
+    },
+  ];
+
+  const [transactionList, setTransactionList] = useState(
+    initialTransactionList
+  );
+
+  const addTransactionHandler = (description, amount) => {
+    setTransactionList((prevState) => {
+      const id = prevState.length * Math.floor(Math.random() * 1000);
+      return [
+        {
+          id: id,
+          description: description,
+          amount: amount,
+          date: formatDate,
+        },
+        ...prevState,
+      ];
+    });
+    console.log(transactionList);
+  };
+
   return (
     <div className="bottom">
-      <AddTransactForm />
+      <AddTransactForm addTransaction={addTransactionHandler} />
       <div className="container">
         <IncomeList />
         <ExpenseList />
